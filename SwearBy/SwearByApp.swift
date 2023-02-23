aece7c0ff1d73c3b7b7717b5d5a599ccaf58d7fe
Colin Power <colinjpower1@gmail.com>
2023-02-23 12:03:56 -0600

    Initial Commit

---
+++ SwearBy/SwearByApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwearByApp.swift
+//  SwearBy
+//
+//  Created by Colin Power on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwearByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
